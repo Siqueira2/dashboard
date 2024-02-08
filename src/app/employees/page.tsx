7bda639a1545d2623c58c365c7ae175b4a2e4d31
Guilherme Siqueira <siqueiraguilherme920@gmail.com>
2024-02-08 00:33:22 -0300

    add navbar

---
+++ src/app/employees/page.tsx
@@ -0,0 +1,9 @@
+const Employees = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Employees
+    </div>
+  );
+};
+
+export default Employees;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+
+        <li>
+          <Link href="/employees">Employees</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
